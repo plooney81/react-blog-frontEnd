306c06472909a62e6302d1981596c1c66405ed84
Pete Looney <petelooney81@gmail.com>
2020-11-07 15:49:49 -0600

    font awesome and color changes

--- src/components/ArticleCard/ArticleCard.js
+++ src/components/ArticleCard/ArticleCard.js
@@ -1,27 +1,54 @@
 import React from 'react';
+import {useState, useEffect} from 'react';
+
 import {Card} from 'react-bootstrap';
 
 import './ArticleCard.css';
 
 export default function ArticleCard(props) {
-    const { title, author, content, createdAt, published } = props.postData
+    const { id, title, author, content, createdAt, published } = props.postData
     const timeCreated = new Date(createdAt);
+    const [comments, setComments] = useState([]);
+
+    useEffect(() => {
+        fetch(`http://127.0.0.1:3000/api/v1/comments/${id}`)
+            .then(res => res.json())
+            .then(data => {
+                setComments(data)
+            })
+            .catch(error => console.log(error))
+    }, [])
     return (
+        <>
         <Card className="mt-4">
-            <Card.Header className="cardTitle">{title}</Card.Header>
+            <Card.Header className="cardTitle">
+                <i class="fas fa-newspaper mr-3"></i>
+                {title}
+            </Card.Header>
             <Card.Body className="d-flex flex-column">
-                <Card.Text>
+                <Card.Text className="p-1">
                     {content}
                 </Card.Text>
-                <div className="d-flex">
-                        <span className="mr-4">{author}</span>
+                <div className="d-flex justify-content-between">
+                    <div className="d-flex">
+                        <span className="mr-4">
+                            <i class="far fa-user-circle mr-1"></i>
+                            {author}
+                        </span>
+                        <span className="mr-4">|</span>
                         <span className="timeCreated">
                             {timeCreated.getDay()}/
                             {timeCreated.getMonth()}/
                             {timeCreated.getFullYear()}
                         </span>
                     </div>
+                    <span className="ml-5">
+                        <i class="far fa-comment mr-2"></i>
+                        {comments.length > 0 ? comments.length : ''}
+                    </span>
+                </div>
             </Card.Body>
         </Card>
+        </>
     )
 }
